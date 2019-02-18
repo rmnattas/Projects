fdd7a589c5d9e346f80df1887a95caf40defd18d
Abdulrahman Alattas <rmnattas@gmail.com>
2019-02-18 00:07:19 -0700

    socket connect but doesn't send yet

--- SocketProgramming/SocketAPI.cpp
+++ SocketProgramming/SocketAPI.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <assert.h>
 #include <sys/errno.h>
+#include <string>
 
 #include <sys/socket.h> 
 #include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <ifaddrs.h> // getIP
+
+#define PORT 9007
 
 using namespace std;
 
@@ -30,33 +35,130 @@ using namespace std;
 //    unsigned char        sin_zero[8];
 // };
 
+sockaddr getIP(){
+    struct ifaddrs *id;
+    int val;
+    val = getifaddrs(&id);
+    printf("Network Interface Name :- %s\n",id->ifa_name);
+    printf("Network Address of %s :- %u\n",id->ifa_name,id->ifa_addr);
+    return *(id->ifa_addr);
+}
+
 void server(){
+
+    // get interface ip
+    //sockaddr IP = getIP();
+    //sockaddr_in *inIP = (struct sockaddr_in *)&IP;
+
+
+    // set address and port
+    sockaddr_in addrport;
+    socklen_t addrlen = (socklen_t) sizeof(addrport);
+    addrport.sin_family = AF_INET;
+    addrport.sin_port = htons(PORT);
+    addrport.sin_addr.s_addr = htonl(INADDR_ANY);
+    //addrport.sin_addr.s_addr = inet_addr(IP);
+    //addrport.sin_addr = inIP->sin_addr;
+    sockaddr* sa = (sockaddr*) &addrport;
+
+    cout << "IP:   " << inet_ntoa(addrport.sin_addr) << endl;
+    cout << "Port: " << ntohs(addrport.sin_port) << endl;
+
+    // set socket
     int sockid = socket(PF_INET, SOCK_STREAM, 0); // set socket
     if (sockid == -1){
         cout << strerror(errno) << endl;
         assert(false);
     }
 
-    sockaddr_in addrport;
-    addrport.sin_family = AF_INET;
-    addrport.sin_port = htons(631);
-    addrport.sin_addr.s_addr = htonl(INADDR_ANY);
-    sockaddr* sa = (sockaddr*) &addrport;
-    int status = bind(sockid, sa, sizeof(addrport)); // bind socket to address port
-    if (status == -1){
+    // bind socket to address port
+    int bind_status = bind(sockid, sa, sizeof(addrport)); 
+    if (bind_status == -1){
         cout << strerror(errno) << endl;
         assert(false);
     }
+
+    // set listening for a connection
+    int listen_status = listen(sockid, 3);
+    if (listen_status == -1){
+        cout << strerror(errno) << endl;
+        assert(false);
+    }
+
+    // listen and accept a connection
+    cout << "Listening..." << endl;
+    int new_socket = accept(sockid, sa, &addrlen);
+    cout << "Connected!" << endl;
+
+    // send a msg
+    string msg;
+    cout << "Message to send: ";
+    cin >> msg;
+    int send_status = send(sockid, &msg, sizeof(msg), 0);
+    if (send_status == -1){
+        cout << strerror(errno) << endl;
+        assert(false);
+    }
+
+
 }
 
 void client(){
 
+    // set address and port
+    sockaddr_in addrport;
+    socklen_t addrlen = (socklen_t) sizeof(addrport);
+    addrport.sin_family = AF_INET;
+    addrport.sin_port = htons(PORT);
+    addrport.sin_addr.s_addr = htonl(INADDR_ANY);
+    sockaddr* sa = (sockaddr*) &addrport;
+
+    // set socket
+    int sockid = socket(PF_INET, SOCK_STREAM, 0); // set socket
+    if (sockid == -1){
+        cout << strerror(errno) << endl;
+        assert(false);
+    }
+
+    // connect to server
+    int conn_status = connect(sockid, sa, addrlen);
+    if (conn_status == -1){
+        cout << strerror(errno) << endl;
+        assert(false);
+    }
+
+    cout << "Connected!" << endl;
+    cout << "Waiting for a message..." << endl;
+
+    // receive msg 
+    string msg;
+    int recv_status = recv(sockid, &msg, 1024, 0);
+    if (recv_status == -1){
+        cout << strerror(errno) << endl;
+        assert(false);
+    }
+
+    cout << msg << endl;
+        
+
 }
 
 int main(){
-    
-    server();
+
+    int op = 0;
+    while (op != 1 and op != 2){
+        cout << "Select:";
+        cout << "    1-Server: to wait for a connection" << endl;
+        cout << "\t   2-Client: to connect to a server" << endl;
+        cout << "Option: ";
+        cin >> op; 
+    }
+
+    if (op == 1)
+        server();
+    else
+        client();
+
 
     return 0;
 }
-
